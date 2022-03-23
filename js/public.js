a0e3db07990b7bed152d5f1fd3337816fbd60080
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-03-23 22:55:16 +0300

    add changes

--- js/public.js
+++ js/public.js
@@ -1,24 +1,41 @@
-(() => {
-    const sec = 1000,
-      min = sec * 60,
-      hour = min * 60,
-      day = hour * 24;  
-  })();
-  const end = new Date("December,31, 2022 12:00:00").getTime();
-  
-  const int = setInterval(() => {
-    const current = new Date().getTime();
-    const remaining = end - current;
-    document.getElementById("days").innerText = Math.floor(remaining / day);
-    document.getElementById("hours").innerText = Math.floor( (remaining % day) / hour );
-    document.getElementById("minutes").innerText = Math.floor( (remaining % hour) / min );
-    document.getElementById("seconds").innerText = Math.floor( (remaining % min) / sec );   
-   1000;if (remaining < 0) {
-    document.querySelector("h1").innerText = "We Have Arrived!";
-    const digit = document.querySelectorAll("span");
-    digit.forEach((digit) => {
-      digit.innerText = "0";
-    });
-    clearInterval(int);
-  }},)
-  
+function updateTimer(deadline) {
+  var time = deadline - new Date();
+  return {
+      'days': Math.floor(time / (1000 * 60 * 60 * 24)),
+      'hours': Math.floor((time / (1000 * 60 * 60)) % 24),
+      'minutes': Math.floor((time / 1000 / 60) % 60),
+      'seconds': Math.floor((time / 1000) % 60),
+      'total': time
+  };
+}
+function animateClock(span) {
+  span.className = "turn";
+  setTimeout(function() {
+      span.className = "";
+  }, 700);
+}
+function startTimer(id, deadline) {
+  var timerInterval = setInterval(function() {
+      var clock = document.getElementById(id);
+      var timer = updateTimer(deadline);
+      clock.innerHTML = '<span>' + timer.days + '</span>' +
+          '<span>' + timer.hours + '</span>' +
+          '<span>' + timer.minutes + '</span>' +
+          '<span>' + timer.seconds + '</span>';
+      //animations
+      var spans = clock.getElementsByTagName("span");
+      animateClock(spans[3]);
+      if (timer.seconds == 59) animateClock(spans[2]);
+      if (timer.minutes == 59 && timer.seconds == 59) animateClock(spans[1]);
+      if (timer.hours == 23 && timer.minutes == 59 && timer.seconds == 59) animateClock(spans[0]);
+      //check for end of timer
+      if (timer.total < 1) {
+          clearInterval(timerInterval);
+          clock.innerHTML = '<span>0</span><span>0</span><span>0</span><span>0</span>';
+      }
+  }, 1000);
+}
+window.onload = function() {
+  var deadline = new Date("June 30, 2023                   2:15:00");
+  startTimer("clock", deadline);
+};
